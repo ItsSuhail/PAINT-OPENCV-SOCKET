13eafed3ed64aafc72c41938a6c20e314997f434
ItsSuhail <82229055+ItsSuhail@users.noreply.github.com>
2021-04-10 11:14:21 +0530

    Add files via upload

---
+++ clntMain.py
@@ -0,0 +1,53 @@
+"""
+Start- 9th April 2021: 6:50 PM
+--Made by Suhail Hasan--
+"""
+#
+"""
+IMPORTING MODULES:
+"""
+import socket
+import cv2
+import pickle
+import struct
+import time
+import sys
+"""
+CLIENT WORK
+"""
+CLIENT_SOCKET = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+HOSTNAME = socket.gethostbyname(socket.gethostname())
+PORT = 5555
+CLIENT_SOCKET.connect((HOSTNAME,PORT))
+"""
+GETTING DATA FROM SERVER
+"""
+data = b""
+payload_size = struct.calcsize("Q")
+try:
+    while True:
+        while len(data) < payload_size:
+            packet = CLIENT_SOCKET.recv(4*1024) # 4K
+            if not packet: break
+            data+=packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q",packed_msg_size)[0]
+        
+        while len(data) < msg_size:
+            data += CLIENT_SOCKET.recv(4*1024)
+        frame_data = data[:msg_size]
+        data  = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        cv2.imshow("PAINT--RECEIVING",frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key  == ord('q'):
+            break
+except socket.error as exc:
+    print("SOCKET ERROR: ",exc)
+    CLIENT_SOCKET.close()
+    time.sleep(10)
+    sys.exit()
+CLIENT_SOCKET.close()
+time.sleep(10)
+sys.exit()
---
+++ srvMain.py
@@ -0,0 +1,158 @@
+"""
+Start- 9th April 2021: 6:50 PM
+--Made by Suhail Hasan--
+"""
+#
+"""
+IMPORTING MODULES:
+"""
+import cv2
+import socket
+import pickle
+import struct
+import numpy
+import threading
+import sys
+import time
+"""
+SERVER WORK:
+"""
+SERVER_SOCKET = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+HOSTNAME = socket.gethostbyname(socket.gethostname())
+PORT = 5555
+print("HOST IP: ",HOSTNAME, " HOST PORT: ", PORT)
+SOCKET_ADDRESS = (HOSTNAME,PORT)
+def start_srv():
+    SERVER_SOCKET.bind(SOCKET_ADDRESS)
+    SERVER_SOCKET.listen(1)
+    print("[SERVER] is now listening")
+
+"""
+DEFINING IMAGE
+"""
+#
+IMAGE = numpy.zeros((512,512,3),numpy.uint8)
+IMAGE[:] = [255,255,255]
+cv2.namedWindow('PAINT')
+"""
+DEFINING IMAGE FOR TRACKBAR
+"""
+#
+IMAGE_TB = numpy.zeros((150,410,3),numpy.uint8)
+cv2.namedWindow('PAINT-COLOR')
+"""
+DEFINING COLORS
+"""
+#
+B = 0
+G = 0
+R = 0
+"""
+DEFINING FUNCTIONS FOR SETTING COLORS
+"""
+#
+def imageColor(img,B,G,R):
+    img[:] = [B,G,R]
+"""
+DEFINING VARIABLES FOR DRAWING
+"""
+#
+DRAW = False
+MODE = True
+SHOW = False
+"""
+DEFINING ONCALLBACK
+"""
+#
+def onCallBack(event,x,y,flags,params):
+    global current_former_x,current_former_y,DRAW, MODE, IMAGE, SHOW
+    if event == cv2.EVENT_RBUTTONDBLCLK:
+        imageColor(IMAGE,255,255,255)
+    if event == cv2.EVENT_RBUTTONDOWN:
+        imageColor(IMAGE,B,G,R)
+    if event == cv2.EVENT_RBUTTONUP or event == cv2.EVENT_LBUTTONUP:
+        SHOW = True
+    if event == cv2.EVENT_LBUTTONDOWN:
+        DRAW = True
+        current_former_x,current_former_y=x,y
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if DRAW == True:
+            if MODE == True:
+                cv2.line(IMAGE,(current_former_x,current_former_y),(x,y),(B,G,R),5)
+                current_former_x = x
+                current_former_y = y
+    elif event==cv2.EVENT_LBUTTONUP:
+        DRAW=False
+        if MODE==True:
+            cv2.line(IMAGE,(current_former_x,current_former_y),(x,y),(B,G,R),5)
+            current_former_x = x
+            current_former_y = y
+    
+    cv2.imshow("PAINT",IMAGE)
+"""
+DEFINING FUNCTION FOR SENDING IMAGE CONTINOUSLY
+"""
+#
+def sendImg(IMAGE,CLIENT):
+    global SHOW
+    PickleIMG = pickle.dumps(IMAGE)
+    Struct_Img = struct.pack("Q",len(PickleIMG))+PickleIMG
+    try:
+        CLIENT.sendall(Struct_Img)
+        SHOW = False
+    except socket.error as exc:
+        print("I think that socket is closed! Error: ",exc)
+        sys.exit()
+        
+"""
+DECLARING FUNCTION TO START THREADING
+"""
+#
+def startThreadSI(IMG,CLNT):
+    thread = threading.Thread(target=sendImg,args=[IMG,CLNT])
+    thread.start()
+"""
+DEFINING NOTHING FUNCTION FOR WORKING OF TRACKBAR
+"""
+#
+def nothing(x):
+    pass
+"""
+ALL DONE, CREATING WHILE LOOP
+"""
+try:
+    start_srv()
+    client_socket,addr = SERVER_SOCKET.accept()
+    print('GOT CONNECTION FROM:',addr)
+except socket.error as exc:
+    print("SOCKET ERROR: ",exc)
+cv2.setMouseCallback("PAINT",onCallBack)
+"""
+DEFINING TRACKBAR
+"""
+cv2.createTrackbar('B','PAINT-COLOR',0,255,nothing)
+cv2.createTrackbar('G','PAINT-COLOR',0,255,nothing)
+cv2.createTrackbar('R','PAINT-COLOR',0,255,nothing)
+"""
+SHOWING THE IMAGES
+"""
+cv2.imshow("PAINT",IMAGE)
+cv2.imshow("PAINT-COLOR",IMAGE_TB)
+try:
+    while True:
+        B = cv2.getTrackbarPos('B','PAINT-COLOR')
+        G = cv2.getTrackbarPos('G','PAINT-COLOR')
+        R = cv2.getTrackbarPos('R','PAINT-COLOR')
+        IMAGE_TB[:] = [B,G,R]
+        cv2.imshow("PAINT-COLOR",IMAGE_TB)
+        if client_socket and SHOW:
+            sendImg(IMAGE, client_socket)
+        k = cv2.waitKey(1)
+        if k & 0xff == ord('q'):
+            break
+except socket.error as exc:
+    print("SOCKET ERROR: ",exc)
+
+cv2.destroyAllWindows()
+time.sleep(10)
+sys.exit()
